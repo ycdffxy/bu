7b9c517dd4a4b08eadd3a9e2fbdfaf59b346afa9
ycdffxy <44016440+ycdffxy@users.noreply.github.com>
2021-03-03 22:23:16 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,6 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+git clone https://github.com/vernesong/OpenClash package/luci-app-openclash
+git clone https://github.com/jerrykuku/luci-theme-argon.git  package/luci-theme-argon
+git clone https://github.com/coolsnowwolf/packages.git
